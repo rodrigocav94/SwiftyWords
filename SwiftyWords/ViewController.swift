4c20758b87e51b050ddf7da2f46ab4f62a5e7e7b
Rodrigo A. Cavalcanti <rodrigocav94@gmail.com>
2024-04-19 04:47:15 -0300

    Initial Commit

---
+++ SwiftyWords/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SwiftyWords
+//
+//  Created by Rodrigo Cavalcanti on 19/04/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
